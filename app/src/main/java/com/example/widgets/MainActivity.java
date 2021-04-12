73f6d7cdddd513d240ec82d7ba4b23a883969ddb
Patrik Silfverberg <b20patsi@student.his.se>
2021-04-12 12:15:54 +0200

    Checkbox set to false when button clicked

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button mainButton = findViewById(R.id.button);
+        final CheckBox checkbox1 = findViewById(R.id.checkBox1);
+        final CheckBox checkbox2 = findViewById(R.id.checkBox2);
+        final CheckBox checkbox3 = findViewById(R.id.checkBox3);
+        final CheckBox checkbox4 = findViewById(R.id.checkBox4);
 
         mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mainButton.setText("Confirmed");
+                checkbox1.setChecked(false);
+                checkbox2.setChecked(false);
+                checkbox3.setChecked(false);
+                checkbox4.setChecked(false);
                 Log.d("==>","We clicked on button!");
             }
         });
